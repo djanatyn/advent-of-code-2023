e49273beca37c23098912e493b5405a3a77ee678
Jonathan Strickland <djanatyn@gmail.com>
2023-12-01 19:55:10 -0500

    day01: wrong part 2 solution

--- rust/src/bin/day01.rs
+++ rust/src/bin/day01.rs
@@ -2,7 +2,9 @@ use std::io;
 
 fn main() -> io::Result<()> {
     let input = include_str!("input/day01/input.txt");
-    Ok(println!("part 1: {}", solve1(input)))
+    println!("part 1: {}", solve1(input));
+    println!("part 2: {}", solve2(input));
+    Ok(())
 }
 
 fn solve1(lines: &str) -> u64 {
@@ -13,17 +15,64 @@ fn solve1(lines: &str) -> u64 {
         .sum()
 }
 
+fn solve2(lines: &str) -> u64 {
+    lines
+        .lines()
+        .map(|line| dbg!(translate_words(line)))
+        .map(|line| find_digits(line.as_str()))
+        .map(CalibrationDigits::combine)
+        .sum()
+}
+
+#[derive(Debug, PartialEq)]
 struct CalibrationDigits(char, char);
 
 fn find_digits(line: &str) -> CalibrationDigits {
     let digits: Vec<char> = line.chars().filter(|c| c.is_ascii_digit()).collect();
-    CalibrationDigits(*digits.first().unwrap(), *digits.last().unwrap())
+    dbg!(CalibrationDigits(
+        *digits.first().unwrap(),
+        *digits.last().unwrap()
+    ))
 }
 
 impl CalibrationDigits {
     fn combine(self) -> u64 {
         let CalibrationDigits(first, last) = self;
         return format!("{first}{last}").parse::<u64>().unwrap();
+    }
+}
+
+const REPLACEMENTS: [(&str, &str); 9] = [
+    ("one", "1"),
+    ("two", "2"),
+    ("three", "3"),
+    ("four", "4"),
+    ("five", "5"),
+    ("six", "6"),
+    ("seven", "7"),
+    ("eight", "8"),
+    ("nine", "9"),
+];
+
+fn translate_words<S: AsRef<str>>(line: S) -> String {
+    let line = line.as_ref();
+    let replacement = REPLACEMENTS.iter().fold(
+        None,
+        |acc: Option<(usize, &str, &str)>, (find, replace): &(&str, &str)| {
+            let idx = match line.find(find) {
+                None => return acc,
+                Some(idx) => idx,
+            };
+            match acc {
+                None => Some((idx, find, replace)),
+                Some((previous_idx, _, _)) if idx < previous_idx => Some((idx, find, replace)),
+                Some((_, _, _)) => acc,
+            }
+        },
+    );
+    match replacement {
+        None => line.to_string(),
+        Some((_, find, replace)) => translate_words(line.replacen(find, replace, 1).as_str()),
     }
 }
 
@@ -36,4 +85,31 @@ fn example01() {
         .map(CalibrationDigits::combine)
         .sum();
     assert_eq!(result, 142);
+}
+
+#[test]
+fn example02() {
+    let example = include_str!("input/day01/example02.txt");
+    let translation: Vec<CalibrationDigits> = example
+        .lines()
+        .map(|line| translate_words(line))
+        .map(|line| find_digits(line.as_str()))
+        .collect();
+    assert_eq!(
+        translation,
+        vec![
+            CalibrationDigits('2', '9'),
+            CalibrationDigits('8', '3'),
+            CalibrationDigits('1', '3'),
+            CalibrationDigits('2', '4'),
+            CalibrationDigits('4', '2'),
+            CalibrationDigits('1', '4'),
+            CalibrationDigits('7', '6'),
+        ]
+    );
+    let result: u64 = translation
+        .into_iter()
+        .map(CalibrationDigits::combine)
+        .sum();
+    assert_eq!(result, 281);
 }
