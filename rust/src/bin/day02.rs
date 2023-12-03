55602ec9f878ea55fe410aba6f92cd52534b138e
Jonathan Strickland <djanatyn@gmail.com>
2023-12-03 03:54:34 -0500

    day02: parse with peg grammar

--- rust/src/bin/day02.rs
+++ rust/src/bin/day02.rs
@@ -1,3 +1,5 @@
+use pest::Parser;
+use pest_derive::Parser;
 use std::io;
 
 fn main() -> io::Result<()> {
@@ -9,6 +11,10 @@ fn main() -> io::Result<()> {
 fn solve1(lines: &str) -> u64 {
     todo!();
 }
+
+#[derive(Parser)]
+#[grammar = "bin/day02.pest"]
+struct GameParser;
 
 #[derive(Debug, PartialEq)]
 enum Cubes {
@@ -26,38 +32,67 @@ struct GameRecord {
     reveals: Vec<Reveal>,
 }
 
-impl GameRecord {
-    fn new<S: AsRef<str>>(input: S) -> Self {
-        let line = input.as_ref();
-        todo!()
+impl TryFrom<&str> for GameRecord {
+    type Error = String;
+
+    fn try_from(line: &str) -> Result<Self, Self::Error> {
+        let mut game = GameParser::parse(Rule::game, line)
+            .map_err(|e| e.to_string())?
+            .next()
+            .ok_or("no games found")?
+            .into_inner();
+        let id = game
+            .next()
+            .ok_or("missing id")?
+            .as_str()
+            .parse::<u64>()
+            .unwrap();
+        let mut reveals = Vec::new();
+        for reveal in game {
+            let mut cubes = Vec::new();
+            for show in reveal.into_inner() {
+                let mut children = show.into_inner();
+                let count = children
+                    .next()
+                    .ok_or("missing count")?
+                    .as_str()
+                    .parse::<u64>()
+                    .map_err(|e| e.to_string())?;
+                let color = match children.next().unwrap().as_str() {
+                    "red" => Cubes::Red(count),
+                    "green" => Cubes::Green(count),
+                    "blue" => Cubes::Blue(count),
+                    _ => Err("invalid color")?,
+                };
+                cubes.push(color);
+            }
+            reveals.push(Reveal(cubes));
+        }
+        Ok(Self { id, reveals })
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use Cubes::{Blue, Green, Red};
+use Cubes::{Blue, Green, Red};
 
-    #[test]
-    fn parse_line() {
-        let line = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
-        let record = GameRecord::new(line);
-        assert_eq!(
-            record,
-            GameRecord {
-                id: 1,
-                reveals: vec![
-                    Reveal(vec![Blue(3), Red(4),]),
-                    Reveal(vec![Red(1), Green(2),]),
-                    Reveal(vec![Blue(6), Green(2)])
-                ]
-            }
-        );
-    }
+#[test]
+fn parse_line() {
+    let line = "Game 123: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+    let record = GameRecord::try_from(line).unwrap();
+    assert_eq!(
+        record,
+        GameRecord {
+            id: 123,
+            reveals: vec![
+                Reveal(vec![Blue(3), Red(4)]),
+                Reveal(vec![Red(1), Green(2), Blue(6)]),
+                Reveal(vec![Green(2)])
+            ]
+        }
+    );
+}
 
-    #[test]
-    fn example01() {
-        let example = include_str!("input/day01/example01.txt");
-        assert_eq!(solve1(example), todo!());
-    }
+#[test]
+fn example01() {
+    let example = include_str!("input/day01/example01.txt");
+    assert_eq!(solve1(example), todo!());
 }
