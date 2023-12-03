759e6b5a759252d3c9542d85a39fffdfc70faad0
Jonathan Strickland <djanatyn@gmail.com>
2023-12-03 04:32:09 -0500

    day02: cleanup

--- rust/src/bin/day02.rs
+++ rust/src/bin/day02.rs
@@ -2,62 +2,6 @@ use pest::Parser;
 use pest_derive::Parser;
 use std::io;
 
-fn main() -> io::Result<()> {
-    let input = include_str!("input/day02/input.txt");
-    println!("part 1: {}", solve1(input));
-    println!("part 2: {}", solve2(input));
-    Ok(())
-}
-
-fn solve1(lines: &str) -> u64 {
-    lines
-        .lines()
-        .map(|line| GameRecord::try_from(line).unwrap())
-        .filter(|record| !GameRecord::invalid(record))
-        .map(|record| record.id)
-        .sum()
-}
-
-fn solve2(lines: &str) -> u64 {
-    lines
-        .lines()
-        .map(|line| GameRecord::try_from(line).unwrap())
-        .map(FewestCubes::from)
-        .map(FewestCubes::power)
-        .sum()
-}
-
-#[derive(Debug)]
-struct FewestCubes {
-    red: u64,
-    green: u64,
-    blue: u64,
-}
-
-impl From<GameRecord> for FewestCubes {
-    fn from(game: GameRecord) -> Self {
-        let mut red: u64 = 0;
-        let mut green: u64 = 0;
-        let mut blue: u64 = 0;
-        for reveal in game.reveals {
-            for cube in reveal.0 {
-                match cube {
-                    Red(count) => red = red.max(count),
-                    Green(count) => green = green.max(count),
-                    Blue(count) => blue = blue.max(count),
-                }
-            }
-        }
-        FewestCubes { red, green, blue }
-    }
-}
-
-impl FewestCubes {
-    fn power(self) -> u64 {
-        self.red * self.green * self.blue
-    }
-}
-
 #[derive(Parser)]
 #[grammar = "bin/day02.pest"]
 struct GameParser;
@@ -76,9 +20,9 @@ impl Cubes {
 
     fn invalid(&self) -> bool {
         match self {
-            Red(count) => count > &Self::NUM_RED_CUBES,
-            Green(count) => count > &Self::NUM_GREEN_CUBES,
-            Blue(count) => count > &Self::NUM_BLUE_CUBES,
+            Self::Red(count) => count > &Self::NUM_RED_CUBES,
+            Self::Green(count) => count > &Self::NUM_GREEN_CUBES,
+            Self::Blue(count) => count > &Self::NUM_BLUE_CUBES,
         }
     }
 }
@@ -140,12 +84,68 @@ impl TryFrom<&str> for GameRecord {
     }
 }
 
-use Cubes::{Blue, Green, Red};
+#[derive(Debug)]
+struct FewestCubes {
+    red: u64,
+    green: u64,
+    blue: u64,
+}
+
+impl From<GameRecord> for FewestCubes {
+    fn from(game: GameRecord) -> Self {
+        let mut red: u64 = 0;
+        let mut green: u64 = 0;
+        let mut blue: u64 = 0;
+        for reveal in game.reveals {
+            for cube in reveal.0 {
+                match cube {
+                    Cubes::Red(count) => red = red.max(count),
+                    Cubes::Green(count) => green = green.max(count),
+                    Cubes::Blue(count) => blue = blue.max(count),
+                }
+            }
+        }
+        FewestCubes { red, green, blue }
+    }
+}
+
+impl FewestCubes {
+    fn power(self) -> u64 {
+        self.red * self.green * self.blue
+    }
+}
+
+fn main() -> io::Result<()> {
+    let input = include_str!("input/day02/input.txt");
+    println!("part 1: {}", solve1(input));
+    println!("part 2: {}", solve2(input));
+    Ok(())
+}
+
+fn solve1(lines: &str) -> u64 {
+    lines
+        .lines()
+        .map(|line| GameRecord::try_from(line).unwrap())
+        .filter(|record| !GameRecord::invalid(record))
+        .map(|record| record.id)
+        .sum()
+}
+
+fn solve2(lines: &str) -> u64 {
+    lines
+        .lines()
+        .map(|line| GameRecord::try_from(line).unwrap())
+        .map(FewestCubes::from)
+        .map(FewestCubes::power)
+        .sum()
+}
 
 #[test]
 fn parse_line() {
     let line = "Game 123: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
     let record = GameRecord::try_from(line).unwrap();
+
+    use Cubes::{Blue, Green, Red};
     assert_eq!(
         record,
         GameRecord {
